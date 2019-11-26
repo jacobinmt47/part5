083681c341d0d9cfe61cd249f1e4608a6d90388a
jacob l <jacobinmt47@outlook.com>
2019-11-26 16:18:34 -0700

    not working

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import BlogEntry from './BlogEntry'
 
 const Blog = ({ blog }) => {
- const b = blog.map(i => <li key={i.id}><BlogEntry url={i.url} author = {i.author} /></li>)
+ const b = blog.map(i => <li key={i.id}><BlogEntry url={i.url} author = {i.author} likes={i.likes} /></li>)
  return(
     <div>
     {b}
--- src/components/BlogEntry.js
+++ src/components/BlogEntry.js
@@ -1,10 +1,28 @@
 import React,{useState, useImperativeHandle}from 'react'
 
 const BlogEntry = React.forwardRef((props,ref) =>{
+    const [visable, setVisable] = useState(false)
+    const hideWhenVisable = {display : visable ? 'none' : ''}
+    const showWhenVisalbe = {display : visable ? '' : 'none'}
+  
+    const ToggleVisibility = () => {
+        setVisable(!visable)
+    }
+    useImperativeHandle(ref,() =>{
+        return ToggleVisibility
+    })
 
     return(
         <div>
-            <a href = {props.url}>{props.title}</a>
+            <div style = {hideWhenVisable} >
+                <button onClick = {ToggleVisibility}>expand</button>
+                <a href = {props.url}>{props.title}</a> <br />
+            </div>
+            <div style = {showWhenVisalbe} >
+                <a href = {props.url}>{props.title}</a> <br />
+                likes: {props.likes} <br />
+                <button onClick = {ToggleVisibility}>contract</button>
+            </div>
         </div>
     )
 })
