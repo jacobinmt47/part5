883a3ad4e55a8c08941080302b5590290b3bf824
jacob l <jacobinmt47@outlook.com>
2019-11-27 12:28:05 -0700

    sort blogs by likes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,6 +2,8 @@ import React from 'react'
 import BlogEntry from './BlogEntry'
 
 const Blog = ({ blog }) => {
+ blog.sort((a,b) => a.likes - b.likes)
+ blog.reverse()
  const b = blog.map(i => <BlogEntry key ={i.id} url={i.url} author = {i.author} likes={i.likes} title ={i.title} id ={i.id}/>)
  return(
     <div>
