9c61691048e89aec6fb90b1c26ad7da0ddeb0006
jacob l <jacobinmt47@outlook.com>
2019-11-30 14:56:30 -0700

    still working on button

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import SimpleBlog from './SimpleBlog'
 
 test('render contents',() => {
@@ -28,4 +29,20 @@ test('render likes',() => {
   const l = component.container.querySelector('.likes')
   expect(l).toHaveTextContent('10')
 
+})
+test('button',() => {
+  const blog = {
+    title:'simple blog',
+    author:'jacob',
+    likes:10
+  }
+  const mockHandler = jest.fn()
+  const component =  render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+  const button = component.container.querySelector('button')
+  fireEvent.click(button)
+  fireEvent.click(button)
+  const l = component.container.querySelector('.likes')
+  expect(l).toHaveTextContent('12')
 })
