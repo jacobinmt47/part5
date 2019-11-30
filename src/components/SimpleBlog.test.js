2c48ca3152daaeab42ac258aa6fb81b31db840a4
jacob l <jacobinmt47@outlook.com>
2019-11-30 14:08:09 -0700

    render likes

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -13,4 +13,19 @@ test('render contents',() => {
     <SimpleBlog blog={blog} />
   )
   expect(component.container).toHaveTextContent('simple blog')
+  expect(component.container).toHaveTextContent('jacob')
+})
+
+test('render likes',() => {
+  const blog = {
+    title:'simple blog',
+    author:'jacob',
+    likes:10
+  }
+  const component =  render(
+    <SimpleBlog blog={blog} />
+  )
+  const l = component.container.querySelector('.likes')
+  expect(l).toHaveTextContent('10')
+
 })
