7651c0f77198a8714793e3320762cb9e97e8e446
jacob l <jacobinmt47@outlook.com>
2019-12-02 14:58:41 -0700

    ?

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 export const useField = (type) => {
-  const [value,setValue] = useState(null)
+  const [value,setValue] = useState('')
   const onChange = (event) => {
     setValue(event.target.value)
   }
